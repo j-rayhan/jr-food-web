4a6e8f9304d0395bde705026bf2a6b918153a11e
johir-rayhan <johir.rayhan01@gmail.com>
2021-10-10 14:48:21 +0600

    Scroll to top done

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+/* eslint-disable jsx-a11y/anchor-has-content */
 import React from 'react';
 import homeImg from '../assets/images/home-img.png';
 import boxImg1 from '../assets/images/s-img-1.jpg';
@@ -202,6 +204,39 @@ const reviews = [
         img: review3,
     },
 ];
+
+const ScrollToTopButton: React.FC = () => {
+    const [visible, setVisible] = React.useState<boolean>(false);
+
+    const toggleVisible = () => {
+        const scrolled = document.documentElement.scrollTop;
+        if (scrolled > 300) {
+            setVisible(true);
+        } else if (scrolled <= 300) {
+            setVisible(false);
+        }
+    };
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+            /* you can also use 'auto' behaviour
+         in place of 'smooth' */
+        });
+    };
+
+    window.addEventListener('scroll', toggleVisible);
+
+    return (
+        <div style={{ display: visible ? 'inline' : 'none' }}>
+            <div onClick={scrollToTop} role="button" aria-hidden>
+                <a href="#home" className="fas fa-angle-up" id="scroll-top" />
+            </div>
+        </div>
+    );
+};
+
 const Main: React.FC = () => {
     const [menuClass, setMenuClass] = React.useState<string>('');
     const [navbarClass, setNavbarClass] = React.useState<string>('');
@@ -416,6 +451,7 @@ const Main: React.FC = () => {
                     created by <span>mr. j-rayhan</span> | all right reserved!
                 </h1>
             </section>
+            <ScrollToTopButton />
         </div>
     );
 };
